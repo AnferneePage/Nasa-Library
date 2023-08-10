2e6b3297580725163a91d87f57107348802e1e76
Latrell Page <pagelatrell14@gmail.com>
2023-08-10 17:28:58 -0500

    Installed flask and set up server

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import requests
 import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 api_key = 'Tz9f45LJnj5G7tnbvwbT5s7InEQ8SVrz4sh0cRjK'
 api_url = 'https://images-api.nasa.gov/search'
+search_query = 'rocks'
 
+@app.route('/')
+def index():
+    params = {'q': search_query}
+    r = requests.get(api_url, params=params)
+    data = r.json()
+    
+    return render_template('static/app.html', api_data=data)
 
-
-# Your search query here
-search_query = 'earth'
-
-params = {
-    'q': search_query
-}
-
-response = requests.get(api_url, params=params)
-data = response.json()
-
-formatted_data = json.dumps(data, indent=20)
-
-print(data)
+if __name__ == '__main__':
+    app.run()
