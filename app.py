02456edb3b3c989e76022c871ed4dfff3a22b5ad
Latrell Page <pagelatrell14@gmail.com>
2023-08-12 12:08:15 -0500

    Finish styling of page, added responsiveness, and added more images and videos, and also rendered html

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
+from website import create_app
 import requests
 import json
-from flask import Flask, render_template
 
-app = Flask(__name__)
 
 api_key = 'Tz9f45LJnj5G7tnbvwbT5s7InEQ8SVrz4sh0cRjK'
 api_url = 'https://images-api.nasa.gov/search'
 search_query = 'rocks'
 
-@app.route('/')
-def index():
-    params = {'q': search_query}
-    r = requests.get(api_url, params=params)
-    data = r.json()
-    
-    return render_template('static/app.html', api_data=data)
+
+app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
