f68cbb0b938248242541ae3e98f59ee14a4dde3a
Latrell Page <pagelatrell14@gmail.com>
2023-08-15 14:37:14 -0500

    Added video functionality

--- app.py
+++ app.py
@@ -12,35 +12,51 @@ headers = {
 }
 
 
-def fetch_images(search_query):
+def fetch_images_and_videos(search_query):
     params = {
-    'q': search_query
+        'q': search_query
     }
 
     response = requests.get(api_url, headers=headers, params=params)
-    data = data = json.loads(response.content)
+    data = json.loads(response.content)
 
-    api_images = []
+    api_media = {'images': [], 'videos': []}
     items = data.get("collection", {}).get("items", [])
     for item in items:
-        links = item["links"] if "links" in item else []
+        media_type = item.get("data", [])[0].get("media_type")
+        nasa_id = item.get("data", [])[0].get("nasa_id")
+        links = item.get("links", [])
         if links:
             preview_link = links[0].get("href")
-            api_images.append(preview_link)
-            print(api_images)
-    return api_images
+            if media_type == "image":
+                api_media['images'].append(preview_link)
+            elif media_type == "video":
+                api_media['videos'].append({'nasa_id': nasa_id, 'preview_link': preview_link})
+    return api_media
+
 
 
 app = create_app()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         search_query = request.form['user-input']
-        print(search_query)
-        api_images = fetch_images(search_query)
-        return render_template('base.html', api_images=api_images, search_query=search_query)
-    return render_template('base.html', api_images=[])
+        api_media = fetch_images_and_videos(search_query)
+        videos_manifests = []
+
+        for video in api_media['videos']:
+            video_nasa_id = video['nasa_id']
+            video_manifest_url = f"https://images-api.nasa.gov/asset/{video_nasa_id}"
+            video_response = requests.get(video_manifest_url)
+            video_manifest_data = json.loads(video_response.content)
+            videos_manifests.append({'nasa_id': video_nasa_id, 'manifest': video_manifest_data})
+            print(videos_manifests)
+
+        return render_template('base.html', api_media=api_media, videos_manifests=videos_manifests, search_query=search_query)
+    return render_template('base.html', api_media={'images': [], 'videos': []})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
