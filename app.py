8c11f722f107456066aaf873f5fb441ffdd246b4
Latrell Page <pagelatrell14@gmail.com>
2023-08-19 22:06:17 -0500

    Fixed api request and module reappear bug

--- app.py
+++ app.py
@@ -22,12 +22,14 @@ def get_apod():
 
     res = requests.get(apod_api_url, params=parameters)
     d = res.json()
-    print(json.dumps(d, indent=4))
+    # print(json.dumps(d, indent=4))
     modal_resources = {'title':'','image':'', 'desciption':''}
     modal_resources['title'] = d['title']
     modal_resources['image'] = d['hdurl']  
     modal_resources['description'] = d['explanation']
     return modal_resources
+
+
 
 
 
@@ -40,23 +42,17 @@ def fetch_images_and_videos(search_query):
     data = json.loads(response.content)
 
     api_media = {'images': [], 'videos': []}
-    seen_video_ids = set()  
-
     items = data.get("collection", {}).get("items", [])
     for item in items:
         media_type = item.get("data", [])[0].get("media_type")
         nasa_id = item.get("data", [])[0].get("nasa_id")
         links = item.get("links", [])
-        
-        if media_type == "image":
+        if links:
             preview_link = links[0].get("href")
-            api_media['images'].append(preview_link)
-        elif media_type == "video":       
-            preview_link = links[0].get("href")
-            # print(preview_link)
-            if preview_link not in seen_video_ids:
+            if media_type == "image":
+                api_media['images'].append(preview_link)
+            elif media_type == "video":
                 api_media['videos'].append({'nasa_id': nasa_id, 'preview_link': preview_link})
-                seen_video_ids.add(preview_link)             
     return api_media
 
 
@@ -66,23 +62,25 @@ def fetch_images_and_videos(search_query):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    modal_resources = get_apod()
+    modal_resources=get_apod()
 
     if request.method == 'POST':
         search_query = request.form['user-input']
         api_media = fetch_images_and_videos(search_query)
         videos_manifests = []
+        seen_links = set()
 
         for video in api_media['videos']:
             video_nasa_id = video['nasa_id']
             video_manifest_url = f"https://images-api.nasa.gov/asset/{video_nasa_id}"
             video_response = requests.get(video_manifest_url)
             video_manifest_data = json.loads(video_response.content)
+            # print(video_manifest_data)
             videos_manifests.append({'nasa_id': video_nasa_id, 'manifest': video_manifest_data})
-            
+            # print(videos_manifests)
             
 
-        return render_template('base.html', modal_resources=modal_resources, api_media=api_media, videos_manifests=videos_manifests, search_query=search_query)
+        return render_template('base.html', api_media=api_media,modal_resources=modal_resources, videos_manifests=videos_manifests, search_query=search_query)
     return render_template('base.html',modal_resources=modal_resources, api_media={'images': [], 'videos': []})
 
 
--- website/static/assets/script.js
+++ website/static/assets/script.js
@@ -1,21 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     var modal = document.getElementById('modal');
     var closeButton = document.getElementById('closeBtn');
+    var logo = document.querySelector('.logo');
 
-    // Open the modal when the page loads
-    modal.style.display = 'block';
+    // Check if the modal should be displayed today
+    var lastVisit = localStorage.getItem('lastVisit');
+    var currentDate = new Date().toDateString();
+
+    if (!lastVisit || lastVisit !== currentDate) {
+        modal.style.display = 'block';
+        localStorage.setItem('lastVisit', currentDate);
+    }
+
+    // Function to open the modal
+    function openModal() {
+        modal.style.display = 'block';
+    }
+
+    // Function to close the modal
+    function closeModal() {
+        modal.style.display = 'none';
+    }
 
     // Close the modal when the close button is clicked
-    closeButton.addEventListener('click', function () {
-        modal.style.display = 'none';
-    });
+    closeButton.addEventListener('click', closeModal);
 
     // Close the modal when the user clicks outside the modal content
     window.addEventListener('click', function (event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
+        if (event.target === modal) {
+            closeModal();
         }
     });
+
+    // Open the modal when the logo is clicked
+    logo.addEventListener('click', openModal);
 });
+
+
 
 
